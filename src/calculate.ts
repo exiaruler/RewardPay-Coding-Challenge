d846763d38b05eca02bbe5f477aa4272df2f81ea
exiaruler <samuelgh.li@gmail.com>
2024-12-07 22:45:22 +1100

    Start of coding challenge, installed TypeScript and ts-node libraries

---
+++ src/calculate.ts
@@ -0,0 +1,10 @@
+import jsonFile from "../data.json";
+
+let results={
+    Revenue:'0',
+    Expenses:'0',
+    GrossProfitMargin:'0%',
+    NetProfitMargin:'0%',
+    WorkingCaptialRatio:'0%'
+};
+console.log(jsonFile);
